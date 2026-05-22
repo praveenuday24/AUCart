ae3456df4efc3f916f028ea8f3eaa460f33296f6
annapooranic <annapoorani1557@gmail.com>
2026-05-22 14:15:26 +0530

    Initial Project setup

---
+++ server/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const http = require('http');
+const mongoose = require('mongoose');
+
+const app = require('./app');
+const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log("MongoDB Connected");
+    
+        server.listen(PORT, ()=>{
+            console.log(`Server running on port ${PORT}`)
+        });
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
